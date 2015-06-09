5ad1b48abdfabdb4b850e5687dfeaaca9180e904
Gustav Larsson <gustav.m.larsson@gmail.com>
2015-06-09 13:37:29 -0500

    Fixed order of imports.

--- ipython_imports.py
+++ ipython_imports.py
@@ -1,6 +1,6 @@
 import numpy as np
 import deepdish as dd
 import itertools as itr
-from matplotlib.pyplot import cm
 from vzlog import VzLog
 import vzlog.pyplot as plt
+from matplotlib.pyplot import cm
